ca6269a60a3fb0a86ccb052cc08bb0aeace7283f
gelsantos19 <absantos@mls.ceu.edu.ph>
2018-02-11 00:13:44 +0800

    Finnished

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   if (isset($_POST['submit'])){
     $task = $_POST['task'];
       if (empty($task)){
-        $errors = "Write your task/s";
+        $errors = "No task found";
       }else{
         mysqli_query($db,"INSERT INTO tasks (task) VALUES ('$task')");
         header('location: index.php');
@@ -20,27 +20,28 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Todo list App</title>
+  <title>To Do List App</title>
   <link rel ="stylesheet" type="text/css" href ="style.css">
 </head>
 <body>
     <div class="header">
-      <h2>Todo list App</h2>
+      <h2>To Do List App</h2>
+      <h5>Write the task/s that you need to do</h5>
     </div>
 
     <form method="POST" action="index.php">
       <?php if(isset($errors)){ ?>
         <p> <?php echo $errors;  ?> <p>
       <?php  } ?>
-      <input type="text" name="task" class="taskbox">
+      <input type="text" name="task" class="taskbox"><br><br>
       <button type="submit" class="addbtn" name="submit"> Add Task</button>
     </form>
 
     <table>
         <thead>
             <tr>
-              <th>Task number</th>
-              <th>Task</th>
+              <th>Task id</th>
+              <th>Task/s</th>
               <th>Action</th>
             <tr>
         <thead>
